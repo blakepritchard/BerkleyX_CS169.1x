16e762423701b17b878c2f0c5b6723e81b543ffe
Blake Pritchard <blakepritchard@yahoo.com>
2015-11-09 01:17:03 +0000

    more refactor

--- app.rb
+++ app.rb
@@ -70,13 +70,22 @@ class HangpersonApp < Sinatra::Base
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+
+    if true # request.host == 'localhost'  then
+      puts "Host=#{request.host}"
+    
+      erb :win 
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if true # request.host == 'localhost' then
+      erb :lose  
+    else
+      redirect '/show'
+    end
   end
   
 end
